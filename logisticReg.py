8b29e27d1132a9b5cc8bbcb324caae7befdaf96c
unknown <drew.kessler.21@bishops.com>
2020-10-07 00:12:14 -0700

    functionalized logistic regression

---
+++ logisticReg.py
@@ -0,0 +1,22 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot
+
+
+
+def logisticReg(data, target, reshape = 0, testProportion = 0.2):
+    # takes numpy array-like arguments, reshape int defaulted at 0 for 1D flattening.
+    # float for train_test_split defaulted at 20%
+    if reshape == 0:
+        reshape = data.shape[1] * data.shape[2]
+
+    data = data.reshape(len(data), reshape)
+
+    trainX, testX, trainY, testY = train_test_split(data,target,test_size = testProportion, shuffle = True)
+
+    regClassifier = LogisticRegression()
+    regClassifier.fit(trainX,trainY)
+    predY = regClassifier.predict(testX)
+
+    sklearn.metrics.plot_confusion_matrix(regClassifier,testX,testY)
+    pyploy.show()
