ad180f2643064c146bf65f2f2535532551b2a57a
unknown <drew.kessler.21@bishops.com>
2020-10-14 00:07:32 -0700

    adjusting numpy array construction to fit rotated images; completed plot_confusion_matrix logisticReg

--- load_data.py
+++ load_data.py
@@ -3,19 +3,29 @@ import cv2
 import os
 
 
-def getDigits(imgFolderPath):
-    digits = np.zeros((208,4128,2322), dtype = 'uint8') # digits.shape[1:] corresponds to greyscale card images
-    targets = np.zeros(216, dtype = 'uint8')
+def getDigits(imgFolderPath, num = 216):
+    digits = np.zeros((num,232, 412), dtype = 'uint8') # digits.shape[1:] corresponds to greyscale card images
+    # digits = [None for a in range(num)] list method
+    targets = np.zeros(num, dtype = 'uint8')
 
     directory = os.fsencode(imgFolderPath)
 
-    for inDex,file in enumerate(os.listdir(directory)):
+    for inDex,file in enumerate(os.listdir(directory)[:num]):
          filename = os.fsdecode(file)
+         # print(filename)
          if filename.endswith(".jpg") and 'W' not in filename:
              # print(os.path.join(directory, filename))
-             image = cv2.imread(filename)
-             digits[inDex] = image.cvtColor(image,cv2.COLOR_BGR2GRAY)
-             targts[inDex] = (inDex // 4) + 1
+             image = cv2.imread(imgFolderPath + "\\" + filename)
+             if image.shape[1] == 4128:
+                 digits[inDex] = cv2.resize(cv2.cvtColor(image,cv2.COLOR_BGR2GRAY), (int(image.shape[1]/10), int(image.shape[0]/10)), interpolation = cv2.INTER_AREA)
+             elif image.shape[1] == 2322: # for alternate shapes, directing towards common digits numpy array
+                 digits[inDex] = cv2.resize(cv2.cvtColor(image,cv2.COLOR_BGR2GRAY), (int(image.shape[0]/10), int(image.shape[1]/10)), interpolation = cv2.INTER_AREA)
+             targets[inDex] = (inDex // 4) + 1
 
 
     return (digits, targets)
+
+
+
+if __name__ == '__main__':
+    (data, targets) = getDigits("C:\\Users\\Tamara\\Desktop\\sklearnpractice\\img",5)
--- logisticReg.py
+++ logisticReg.py
@@ -1,6 +1,8 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix
 from matplotlib import pyplot
+from load_data import getDigits
 
 
 
@@ -18,5 +20,11 @@ def logisticReg(data, target, reshape = 0, testProportion = 0.2):
     regClassifier.fit(trainX,trainY)
     predY = regClassifier.predict(testX)
 
-    sklearn.metrics.plot_confusion_matrix(regClassifier,testX,testY)
+    plot_confusion_matrix(regClassifier,testX,testY)
     pyplot.show()
+
+
+
+if __name__ == '__main__':
+    data, target = getDigits("C:\\Users\\Tamara\\Desktop\\sklearnpractice\\img",num = 30)
+    logisticReg(data,target)
